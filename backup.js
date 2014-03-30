f2981e7e605b9750c95f55167e2b162c97d7b2f8
xiuxu123@livecn <xiuxu123@live.cn>
2014-03-30 11:44:37 +0800

    *

--- backup.js
+++ backup.js
@@ -1,5 +1,6 @@
 var process = require('child_process');
 var fs = require('fs');
+var nodemailer = require("nodemailer");
 
 function back(){
 	var date = new Date();
@@ -32,7 +33,7 @@ fs.watch('bak', function (event, filename) {
 	if(event == 'change'){
 		if (filename) {
 		    console.log('filename provided: ' + filename);
-		    upload();
+		    upload(filename);
 		} else {
 		    console.log('filename not provided');
 		}
@@ -40,46 +41,52 @@ fs.watch('bak', function (event, filename) {
 
 });
 
-function upload(){
+function upload(filename){
 
 	var date = new Date();
 	var time = date.getFullYear() + "-" + (date.getMonth()+1) + "-"  + date.getDate() + " "
 		+ date.getHours() + ":"  + date.getMinutes(); 
 
-	process.exec('git add *', function (error, stdout, stderr) {
-
-
-		console.log("git add");
-
-
-	    if (error !== null) {
-	    	console.log('git add * 出错，错误信息：' + error);
-	    }else{
-	    	process.exec('git commit -m "'+time+'"', function (error, stdout, stderr) {
-	    		console.log("git commit");
-
-			    if (error !== null) {
-			    	console.log('git commit 出错，错误信息：' + error);
-			    }else{
-			    		console.log("git push1");
-			    	process.exec('git push -u origin master', function (error, stdout, stderr) {
-			    		console.log("git push2");
-
-					    if (stdout != null) {
-					    	console.log(stdout);
-
-					    	process.exec('xiuxu123@live.cn', function (error, stdout, stderr) {
-					    		console.log("git mail");
-
-							    if (stdout != null) {
-							    	console.log(stdout);
-							    }
-							});
-					    }
-					});
-			    }
-			});
+	// create reusable transport method (opens pool of SMTP connections)
+	var smtpTransport = nodemailer.createTransport("SMTP",{
+	    service: "QQ",
+	    auth: {
+	        user: "171373243@qq.com",
+	        pass: "307zhanghanyun"
 	    }
 	});
+
+	//var files = fs.readFileSync("bak/" + filename)
+
+	// setup e-mail data with unicode symbols
+	var mailOptions = {
+	    from: "171373243@qq.com", // sender address
+	    to: "xiuxu123@live.cn", // list of receivers
+	    subject: "LifeBill备份：" + time, // Subject line
+	    text: "LifeBill备份" + time, // plaintext body
+	    attachments: [
+	        {   // stream as an attachment
+	            fileName: "text4.txt",
+	            streamSource: fs.createReadStream("bak/" + filename)
+	        },
+	        {   // file on disk as an attachment
+	            fileName: filename,
+	            filePath: "/bak/" + filename // stream this file
+	        }
+	    ]
+	}
+
+	// send mail with defined transport object
+	smtpTransport.sendMail(mailOptions, function(error, response){
+	    if(error){
+	        console.log(error);
+	    }else{
+	        console.log("Message sent: " + response.message);
+	    }
+
+	    // if you don't want to use this transport object anymore, uncomment following line
+	    //smtpTransport.close(); // shut down the connection pool, no more messages
+	});
+	
 
 }
